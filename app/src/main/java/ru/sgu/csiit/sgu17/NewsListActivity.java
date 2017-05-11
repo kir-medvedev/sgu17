7a1a1330c19875343dc32897d348cb78df7af8ca
Kirill Medvedev <medvedevke@gmail.com>
2017-05-11 10:18:59 +0400

    Code cleanup

--- app/src/main/java/ru/sgu/csiit/sgu17/NewsListActivity.java
+++ app/src/main/java/ru/sgu/csiit/sgu17/NewsListActivity.java
@@ -1,21 +1,8 @@
 package ru.sgu.csiit.sgu17;
 
 import android.app.Activity;
-import android.app.LoaderManager;
-import android.content.Intent;
-import android.content.Loader;
 import android.content.res.Configuration;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class NewsListActivity extends Activity
         implements NewsListFragment.Listener {
